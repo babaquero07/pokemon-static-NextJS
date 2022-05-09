397f2b8000487f29e4dcb75fc3b6f71531a25cc7
babaquero07 <brayanalexanderb@gmail.com>
2022-05-09 15:57:21 -0500

    Next - getStaticPaths

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,18 +1,43 @@
+import { NextPage, GetStaticProps, GetStaticPaths } from "next";
 import { useRouter } from "next/router"
 import { Layout } from "../../components/layouts"
 
 interface Props {
-  pokemon: any
+  id: string;
+  name: string
 }
 
-const PokemonPage = () => {
-  const {id} = useRouter().query;
+const PokemonPage: NextPage<Props> = ({id, name}) => {
 
   return (
     <Layout title="Algun pokemon">
-        <h1>{id}</h1>
+        <h1>{id} - {name}</h1>
     </Layout>
   )
+}
+
+// You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+
+  return {
+    paths: [
+      {
+        params: {id: '1'}
+      }
+    ],
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  // const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+
+  return {
+    props: {
+      id: 1,
+      name: 'Bulbasaur'
+    }
+  }
 }
 
 export default PokemonPage
