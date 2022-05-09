1aa049b605fab3da9e70e0c9f3ca30ca918065ce
babaquero07 <brayanalexanderb@gmail.com>
2022-05-09 11:29:20 -0500

    getStaticProps. Carga de forma estatica todos los pokemones en mi index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,39 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps } from 'next'
+import { pokeApi } from '../api';
 import { Layout } from '../components/layouts'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+
   return (
     <Layout title='Listado de pokemones'>
-      <h1>Working :)</h1>
+      <ul>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+      </ul>
     </Layout>
   )
 }
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const { data } = await pokeApi.get('/pokemon?limit=151')
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
+}
+
 
 export default Home
