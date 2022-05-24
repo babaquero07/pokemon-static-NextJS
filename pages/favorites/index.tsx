bc8586a7bad5630788fa47ad48c5c53f6a544592
babaquero07 <brayanalexanderb@gmail.com>
2022-05-24 12:29:13 -0500

    Pantalla en favoritos

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,9 +1,21 @@
+import { useState, useEffect } from 'react';
 import { Layout } from '../../components/layouts'
+import {NoFavorites} from '../../components/ui'
+import { localFavorites } from '../../utils';
+
 
 const Favorites = () => {
+  const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemons(localFavorites.pokemons())
+
+  }, [])
+
+
   return (
     <Layout title='Pokemons - Favoritos'>
-        <h1>Favoritos</h1>
+        {favoritePokemons[0] ? (<h1>holi</h1>) : (<NoFavorites/>)}
     </Layout>
   )
 }
--- utils/localFavorites.ts
+++ utils/localFavorites.ts
@@ -20,7 +20,11 @@ const existInFavorites = (id: number): boolean => {
     return favorites.includes(id);
 }
 
+const pokemons = (): number[] => JSON.parse(localStorage.getItem('favorites') || '[]')
+
+
 export default {
     toggleFavorite,
-    existInFavorites
+    existInFavorites,
+    pokemons,
 }
