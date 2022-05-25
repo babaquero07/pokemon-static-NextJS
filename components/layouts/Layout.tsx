83a913bcd11096a290467001b647ea71da01847c
babaquero07 <brayanalexanderb@gmail.com>
2022-05-25 11:52:10 -0500

    Chore: Open Graph Meta Tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,15 +1,22 @@
 import { FC, PropsWithChildren } from "react"
+
 import Head from "next/head"
 import { NavBar } from '../ui';
 
+const origin = (typeof window === 'undefined') ? "" : window.location.origin;
+
 export const Layout: FC<PropsWithChildren<{title:string}>> = ({ children, title}) => {
+
   return (
     <>
         <Head>
             <title>{title || 'Pokemon App'}</title>
             <meta name="author" content='Brayan Baquero'/>
-            <meta name="description" content='Informacion sobre el pokemon XXXX'/>
-            <meta name="keywords" content='XXX, pokemon, pokedex'/>
+            <meta name="description" content='Informacion sobre un pokemon'/>
+            <meta name="keywords" content='pokeapi, pokemon, pokedex'/>
+            <meta property="og:title" content={`Informacion sobre el pokemon ${title}`} />
+            <meta property="og:description" content={`Esta es la pagina sobre ${title}`} />
+            <meta property="og:image" content={`${origin}/img/banner.png`} />
         </Head>
 
         <NavBar/>
